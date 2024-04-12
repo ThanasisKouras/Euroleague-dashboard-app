7000d2c9963e51eafc27463eb74b1f563f99c32d
Athanasios Kouras <88839122+ThanasisKouras@users.noreply.github.com>
2024-04-12 19:48:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ body {
 
 # Additional imports for API calls
 from euroleague_api import standings
-from euroleague_api.player_stats import get_player_stats_single_season
-from euroleague_api.team_stats import get_team_stats_single_season
+from euroleague_api import get_player_stats_single_season
+from euroleague_api import get_team_stats_single_season
 
 
 
